3410a2af60f91ae3d37aa5b7c0b6aadc3e64620e
Jonatas Benicio <jonathas_santos93@hotmail.com>
2024-02-13 23:38:30 -0300

    :bug: fix: utilizando o transacional correto

--- src/main/java/com/jb/erp/util/ServiceUtils.java
+++ src/main/java/com/jb/erp/util/ServiceUtils.java
@@ -1,27 +1,24 @@
 package com.jb.erp.util;
 
 import java.io.Serializable;
-
 import javax.inject.Inject;
-import javax.transaction.Transactional;
-
 import com.jb.erp.model.User;
 import com.jb.erp.repository.UsersSearch;
 
-public class ServiceUtils implements Serializable{
+public class ServiceUtils implements Serializable {
 	private static final long serialVersionUID = 1L;
-	
+
 	@Inject
 	UsersSearch userSearch;
-	
-	@Transactional
+
+	@Transacional
 	public void salvarUser(User usuario) {
 		userSearch.Save(usuario);
 	}
-	
-	@Transactional
+
+	@Transacional
 	public User findLoginUser(String login, String Senha) {
-		
+
 		return userSearch.findUserLogin(login, Senha);
 	}
 }
