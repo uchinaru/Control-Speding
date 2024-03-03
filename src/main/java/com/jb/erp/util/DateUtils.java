ba2bab3ed6f87e9529c05bc60ca92fef1bfd89e2
Jonatas Benicio <jonathas_santos93@hotmail.com>
2024-03-03 18:44:31 -0300

    feat: aplica novo metodo de tratar datas simples

--- src/main/java/com/jb/erp/util/DateUtils.java
+++ src/main/java/com/jb/erp/util/DateUtils.java
@@ -8,16 +8,27 @@ import java.util.Date;
 public class DateUtils implements Serializable{
 	private static final long serialVersionUID = 1L;
 	
-	private static SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss");
+	private static SimpleDateFormat sdfhora = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss");
+	private static SimpleDateFormat sdfsimples = new SimpleDateFormat("dd/MM/yyyy");
 	
 	public Date dateTimeStampFormat() {
 	       Timestamp timeStamp = new Timestamp(System.currentTimeMillis());
-	        String formattedDate = sdf.format(timeStamp);
+	        String formattedDate = sdfhora.format(timeStamp);
 	        try {
-	            return sdf.parse(formattedDate);
+	            return sdfhora.parse(formattedDate);
 	        } catch (Exception e) {
 	            e.printStackTrace();
 	            return null;
 	        }
 	} 
+	
+	public Date transformaDataSimples(Date date) {
+		String formattedDate = sdfsimples.format(date);
+		try {
+			return sdfsimples.parse(formattedDate);
+		} catch (Exception e) {
+			 return null;
+		}
+	}
+	
 }
