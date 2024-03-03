a31f6062a2b47db1446da385fd14a0dab6740cba
Jonatas Benicio <jonathas_santos93@hotmail.com>
2024-03-03 18:55:03 -0300

    feat: aplica metodo para recuperar/exibir data completa

--- src/main/java/com/jb/erp/util/DateUtils.java
+++ src/main/java/com/jb/erp/util/DateUtils.java
@@ -8,14 +8,14 @@ import java.util.Date;
 public class DateUtils implements Serializable{
 	private static final long serialVersionUID = 1L;
 	
-	private static SimpleDateFormat sdfhora = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss");
+	private static SimpleDateFormat sdfComHora = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss");
 	private static SimpleDateFormat sdfsimples = new SimpleDateFormat("dd/MM/yyyy");
 	
 	public Date dateTimeStampFormat() {
 	       Timestamp timeStamp = new Timestamp(System.currentTimeMillis());
-	        String formattedDate = sdfhora.format(timeStamp);
+	        String formattedDate = sdfComHora.format(timeStamp);
 	        try {
-	            return sdfhora.parse(formattedDate);
+	            return sdfComHora.parse(formattedDate);
 	        } catch (Exception e) {
 	            e.printStackTrace();
 	            return null;
@@ -26,6 +26,15 @@ public class DateUtils implements Serializable{
 		String formattedDate = sdfsimples.format(date);
 		try {
 			return sdfsimples.parse(formattedDate);
+		} catch (Exception e) {
+			 return null;
+		}
+	}
+	
+	public Date transformaDataCompleta(Date date) {
+		String formattedDate = sdfComHora.format(date);
+		try {
+			return sdfComHora.parse(formattedDate);
 		} catch (Exception e) {
 			 return null;
 		}
